493c92b4659c5dcb36de053a6384c4299063d96e
lankongclub <439891354@qq.com>
2018-10-15 11:59:40 +0800

    Mod: new dist

---
+++ dist/precache-manifest.ad86428fd9fc995b3b004c3f4b5a1ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1a76236574c8a91095ca",
+    "url": "/js/chunk-vendors.3b3d3278.js"
+  },
+  {
+    "revision": "31120d985aba79734af1",
+    "url": "/js/app.b0c28687.js"
+  },
+  {
+    "revision": "905794d5431777be34be",
+    "url": "/js/about.878c9c1f.js"
+  },
+  {
+    "revision": "28d6f2beffea94f5b058faaf7d7b8abb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "31120d985aba79734af1",
+    "url": "/css/app.61555e0d.css"
+  }
+];
